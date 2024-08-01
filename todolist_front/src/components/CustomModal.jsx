185a45cba5cec90dd7ff2a64589deffc41f1e9d0
CodeDDAGGI <qudrb1132@naver.com>
2024-08-01 10:57:28 +0900

    0801

---
+++ todolist_front/src/components/CustomModal.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+
+function CustomModal() {
+    const [ isModal , setModal] = useState(false);
+
+    
+    
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default CustomModal;
